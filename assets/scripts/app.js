454951b580e0fe6312e7ec220c6064a1bdb685bc
Marius CA <marius@example.com>
2023-03-01 10:52:50 +0200

    open movie modal works

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,22 @@
 'use strict';
 console.log('app.js file was loaded');
+
+// Taikomes
+
+const els = {
+  addMovieBtn: document.getElementById('addMovieBtn'),
+  addMovieModal: document.getElementById('add-modal'),
+  backdropEl: document.getElementById('backdrop'),
+};
+console.log('els ===', els);
+
+// Event listeners
+els.addMovieBtn.addEventListener('click', showAddMovieModal);
+
+// Functions
+
+function showAddMovieModal() {
+  console.log('showAddMovieModal');
+  els.addMovieModal.classList.add('visible');
+  els.backdropEl.classList.add('visible');
+}
